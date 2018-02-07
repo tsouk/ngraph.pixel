6ca5af23273702cf74f1b6f09676b425f8634e34
Nikolas Tsouk <tsouk.n@gmail.com>
2018-02-07 00:16:44 +0000

    Some more refactoring

--- demo/basic/index.three.js
+++ demo/basic/index.three.js
@@ -3,7 +3,6 @@ const recurseBF = require('../../lib/recurseBFngraph-three');
 const eventify = require('ngraph.events');
 const nthree = require('ngraph.three');
 const THREE = require('three');
-
 const MAX_CHILDREN_PER_NODE = 12;
 
 // Configure
@@ -16,26 +15,16 @@ var physicsSettings = {
   timeStep: 10
 };
 
-
 function start3dgraph (data) {
-  // What is the max number of nodes?
   let maxDepth = recurseBF.findMaxDepth(recurseBF.getHtmlNode(data), MAX_CHILDREN_PER_NODE);
   let maxParticleCount = recurseBF.getChildrenCount();
 
   var layout3d = require('ngraph.forcelayout3d');
-  var layout2d = layout3d.get2dLayout; // this on it's own is not enough, you need to tell the rendering function to set the z=0
+  var layout2d = layout3d.get2dLayout;
   var graphics = nthree(graph, {physicsSettings : physicsSettings, layout: layout2d(graph, physicsSettings)}, maxParticleCount, maxDepth);
-  const threeGraphics = require('../../lib/threeGraphics')(graph, graphics.scene);
-  graphics.createNodeUI(threeGraphics.createNodeUI);
-  graphics.createLinkUI(threeGraphics.createLinkUI);
-  graphics.renderNode(threeGraphics.nodeRenderer);
-  graphics.renderLink(threeGraphics.linkRenderer);
 
-  graphics.scene.fog = new graphics.THREE.FogExp2( 0xccccee, 0.001 );
-  graphics.scene.background = new graphics.THREE.Color( 0xeeeeee );
   graphics.run(); // begin animation loop
-  //graphics.setMaxDepth(maxDepth);
-  //graphics.setMaxParticleCount(recurseBF.getChildrenCount());
+
   recurseBF.recurseBF(graph, recurseBF.getHtmlNode(data), MAX_CHILDREN_PER_NODE);
   
   recurseBF.events.on('added', function( parentNodeId, childNodeId ) {
@@ -44,12 +33,13 @@ function start3dgraph (data) {
 
   recurseBF.events.on('cleared', function() {
     console.log(`Finished adding nodes, stable, maxDepth: ${maxDepth}`);
-    graphics.isStable();
+    setTimeout(() => {
+      graphics.isStable();
+    }, 1000);   
   });
 }
 
 if (window) {
   window.start3dgraph = start3dgraph;
-  //window.scene = graphics.scene;
   window.THREE = THREE;
 }
